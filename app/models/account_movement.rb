30ba3fa4610c84029e6962f87566b7d4ba3a61fd
Christian Hein <chrishein@gmail.com>
2016-08-22 22:45:15 -0300

    Improve amounts import handling

--- app/models/account_movement.rb
+++ app/models/account_movement.rb
@@ -12,20 +12,23 @@ class AccountMovement < ActiveRecord::Base
 
   default_scope { order('movement_date DESC') }
 
+  THOUSANDS_SEPARATOR = ','.freeze
+  NUMERIC_FIELDS = [:credit, :debit, :balance].freeze
+
   def self.import(account, uploaded_file)
     file = uploaded_file.tempfile
     filename = uploaded_file.original_filename
     if filename.end_with?('.csv')
       CSV.foreach(file, headers: true) do |row|
         movement_hash = row.to_hash
-        AccountMovement.create!(movement_hash.merge(:account => account))
+        AccountMovement.create!(prepare_movement(movement_hash, account))
       end
     elsif filename.end_with?('.xls') || filename.end_with?('.xlsx')
       spreadsheet = Roo::Excelx.new(uploaded_file.path)
       header = spreadsheet.row(1)
       (2..spreadsheet.last_row).each do |i|
         movement_hash = Hash[[header, spreadsheet.row(i)].transpose]
-        AccountMovement.create!(movement_hash.merge(:account => account))
+        AccountMovement.create!(prepare_movement(movement_hash, account))
       end
     else
       raise 'Unknown file format'
@@ -33,6 +36,18 @@ class AccountMovement < ActiveRecord::Base
   end
 
   private
+
+    def self.prepare_movement(movement_hash, account)
+      movement_hash = movement_hash.symbolize_keys
+      movement_hash[:account] = account
+      movement_hash.select { |k, v| NUMERIC_FIELDS.include?(k) }.each do |k, v|
+        unless movement_hash[k].nil?
+          movement_hash[k] = movement_hash[k].gsub(THOUSANDS_SEPARATOR, '').to_f
+        end
+      end
+
+      movement_hash
+    end
 
     def credit_xor_debit
       unless credit.blank? ^ debit.blank?
