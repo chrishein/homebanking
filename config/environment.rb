d456c1c701dfcf9455fcb4c6554ba091e8547ccf
Christian Hein <chrishein@gmail.com>
2016-08-25 23:11:05 -0300

    Configure Action Mailer through env vars

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['HB_EMAIL_USERNAME'],
+  :password => ENV['HB_EMAIL_PASSWORD'],
+  :domain => ENV['HB_EMAIL_DOMAIN'],
+  :address => ENV['HB_EMAIL_SMTP_ADDRESS'],
+  :port => ENV['HB_EMAIL_SMTP_PORT'],
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
